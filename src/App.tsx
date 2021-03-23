05abf1ce89f4942aaa20236c7460cf16a0ce9dd7
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-23 18:54:21 -0400

    Created styled Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Calculator from './components/Calculator';
+import styled from 'styled-components';
 import './App.css';
+
+const Container = styled.div`
+  display: flex;
+  min-height: 100vh;
+  width: 100vw;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <Container>
       <Calculator />
-    </div>
+    </Container>
   );
 }
 
