d8b0ffb085320ae80081e4283bde7bec85f7eb9c
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-23 17:12:24 -0400

    Imported component to display

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Calculator from './components/Calculator';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Calculator />
     </div>
   );
 }
