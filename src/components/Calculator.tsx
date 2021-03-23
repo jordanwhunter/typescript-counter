816b53aa98f6312f116d701d80945ec7a010f873
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-23 17:12:12 -0400

    Created calculator component

---
+++ src/components/Calculator.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Calculator: React.FC<{}> = () => {
+  return (
+    <div>
+      Hello World! I am the Calculator Component.
+    </div>
+  )
+};
+
+export default Calculator;
