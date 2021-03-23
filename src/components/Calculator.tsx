b87f1ef8460be4da9436ce82b6ec4adce9ed3e2c
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-23 18:54:49 -0400

    Imported Button component

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import Button from './Button';
 import styled from 'styled-components';
 
 const Container = styled.div`
   background: #323232;
+  flex: 1;
+`;
+
+const Grid = styled.div`
+  display: grid;
 `;
 
 const Calculator: React.FC<{}> = () => {
   return (
     <Container>
       Hello World! I am the Calculator Component.
+      <Grid>
+        <Button />
+      </Grid>
     </Container>
   )
 };
