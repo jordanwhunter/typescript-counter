5c88d98199397fb155f4536e39f7daf8ffca8e81
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-24 11:43:14 -0400

    Added optional position and width prop types

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,18 +2,31 @@ import React from 'react';
 import styled from 'styled-components';
 
 type ButtonProps = {
-  label: string,
+  label: string;
+  // ↓↓↓ Look Ma! a TUPLE in the optional position prop!
+  position?: [x: number, y: number];
+  width?: number;
 };
 
 const StyledButton = styled.button`
   background-color: #e48900;
   border: none;
   border-radius: 8px;
+  font-size: 24px;
 `;
 
-const Button: React.FC<ButtonProps> = ({ label }) => {
+const Button: React.FC<ButtonProps> = ({ label, position, width }) => {
+  const styles: React.CSSProperties = {};
+  if (position) {
+    styles.gridColumnStart = position[0] + 1;
+    styles.gridRowStart = position[1] + 1;
+  }
+  if (width) {
+    styles.gridColumnEnd = `span ${width}`;
+  }
+  
   return (
-    <StyledButton>
+    <StyledButton style={styles}>
       {label}
     </StyledButton>
   )
