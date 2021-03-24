0e8aee771ea3e65691ac6dde7fa6d023c54ca438
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-24 12:09:23 -0400

    Compensated for enum ButtonType and added to IF gate

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 
+export enum ButtonType {
+  Number,
+  Operation
+};
+
 type ButtonProps = {
   label: string;
   // ↓↓↓ Look Ma! a TUPLE in the optional position prop!
   position?: [x: number, y: number];
   width?: number;
   height?: number;
+  type?: ButtonType;
 };
 
 const StyledButton = styled.button`
-  background-color: #e48900;
+  background-color: #727171;
   border: none;
   border-radius: 8px;
   font-size: 24px;
+  color: #fff;
 `;
 
-const Button: React.FC<ButtonProps> = ({ label, position, width, height }) => {
+const Button: React.FC<ButtonProps> = ({ label, position, width, height, type = ButtonType.Operation }) => {
   const styles: React.CSSProperties = {};
   if (position) {
     styles.gridColumnStart = position[0] + 1;
@@ -27,6 +34,10 @@ const Button: React.FC<ButtonProps> = ({ label, position, width, height }) => {
   }
   if (height) {
     styles.gridRowEnd = `span ${height}`;
+  }
+  if (type === ButtonType.Number) {
+    styles.color = '#000';
+    styles.background = '#e48900';
   }
   
   return (
