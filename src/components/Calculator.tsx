5f1586faeaa4071777519cb3f687e81bdb9fa903
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-24 11:43:37 -0400

    Positioning buttons on grid

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -17,16 +17,17 @@ const Calculator: React.FC<{}> = () => {
   return (
     <Container>
       <Grid>
-        <Button label="1" />
-        <Button label="2" />
-        <Button label="3" />
-        <Button label="4" />
-        <Button label="5" />
-        <Button label="6" />
-        <Button label="7" />
-        <Button label="8" />
-        <Button label="9" />
-        
+        <Button label='AC' position={[0, 0]} width={2} />
+        <Button label='?' position={[2, 0]} width={2} />
+        <Button label='9' position={[2, 1]} />
+        <Button label='8' position={[1, 1]} />
+        <Button label='7' position={[0, 1]} />
+        <Button label='6' position={[2, 2]} />
+        <Button label='5' position={[1, 2]} />
+        <Button label='4' position={[0, 2]} />
+        <Button label='3' position={[2, 3]} />
+        <Button label='2' position={[1, 3]} />
+        <Button label='1' position={[0, 3]} />
       </Grid>
     </Container>
   )
