7b65f9057b4e04865f61f70d2060be883f55b2c9
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-24 15:46:01 -0400

    Switch case for Addition, Subtraction or Equals

--- src/modules/calc.ts
+++ src/modules/calc.ts
@@ -91,6 +91,20 @@ export const getOperationsBuilder = (inputs: Array<CalcInput>): OperationsBuilde
   );
 };
 
+const getTotal = (operations: Array<Operation>): number =>
+  operations.reduce<number>((sum, operation) => {
+    switch(operation.operator) {
+      case OperatorType.Add:
+        return sum + operation.value;
+
+      case OperatorType.Subtract:
+        return sum - operation.value;
+
+      case OperatorType.Equals:
+        return sum;
+    }
+  }, 0);
+
 const getState = (inputs: Array<CalcInput>): CalcState => {
   const builder = getOperationsBuilder(inputs);
   const { operations } = builder;
@@ -100,9 +114,7 @@ const getState = (inputs: Array<CalcInput>): CalcState => {
   if (!lastOperation) return {displayValue: 0};
   switch (lastOperation.operator) {
     case OperatorType.Equals:
-      const total = operations.reduce<number>((sum, operation) => sum + operation.value, 0);
-
-      return { displayValue: total }
+      return { displayValue: getTotal(operations) }
     
     default:
       return { displayValue: builder.working.value };
